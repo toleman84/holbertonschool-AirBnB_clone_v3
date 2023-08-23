a9c82eb8fc5b4726ee7c07bd4933bfe337c05afa
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-23 14:02:26 -0300

    fix db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,10 +3,21 @@
 Contains the class DBStorage
 """
 
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from os import getenv
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class DBStorage:
@@ -16,7 +27,6 @@ class DBStorage:
 
     def __init__(self):
         """Instantiate a DBStorage object"""
-        from models.base_model import BaseModel, Base
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
@@ -31,17 +41,7 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """doc"""
-        from models.amenity import Amenity
-        from models.base_model import BaseModel, Base
-        from models.city import City
-        from models.place import Place
-        from models.review import Review
-        from models.state import State
-        from models.user import User
-        classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-        
+        """query on the current database session"""
         new_dict = {}
         for clss in classes:
             if cls is None or cls is classes[clss] or cls is clss:
@@ -66,7 +66,6 @@ class DBStorage:
 
     def reload(self):
         """reloads data from the database"""
-        from models.base_model import BaseModel, Base
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
