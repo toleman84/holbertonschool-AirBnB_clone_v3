dc09be5907502f45580c1c0f747697329219d636
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-22 23:35:38 -0300

    fix: status app /api/v1/status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
-    """doc"""
+    """status app"""
     return jsonify({'status': 'OK'})
