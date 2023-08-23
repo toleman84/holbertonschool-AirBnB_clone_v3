ff8b4abf1fa5c435bd0e4baabaeaadb455c4b46a
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-23 10:25:49 -0300

    add exercise 6. Not found: handler for 404 errors that returns a JSON-formatted 404 status

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """doc"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def teardown(error):
     """error 404"""
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(e):
+    """Handle 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
